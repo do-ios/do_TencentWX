42282b966b53325f8b4be40d56cea137cb6b26f7
yangzhen <1784054000@qq.com>
2016-06-07 10:42:13 +0800

    修改配置，需要注册appid,否则调用是否安装微信app的api不能使用

--- doExtLib/do_TencentWX_App.m
+++ doExtLib/do_TencentWX_App.m
@@ -9,6 +9,9 @@
 #import "do_TencentWX_App.h"
 #import "WXApi.h"
 #import "doScriptEngineHelper.h"
+#import "doServiceContainer.h"
+#import "doIModuleExtManage.h"
+
 static do_TencentWX_App* instance;
 
 @implementation do_TencentWX_App
@@ -24,7 +27,8 @@ static do_TencentWX_App* instance;
 -(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     //注册下个版本在加
-//    [WXApi registerApp:@"wxc422eccce175b3e1"];
+    NSString *weixinAppid = [[doServiceContainer Instance].ModuleExtManage GetThirdAppKey:@"tencentWX" :@"WXAppID" ];
+    [WXApi registerApp:weixinAppid];
     
     return YES;
 }
